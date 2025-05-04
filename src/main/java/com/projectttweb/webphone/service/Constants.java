012a611c6d164444223134666f1b8f1856cb5fbc
Trần Anh Duy <22130063@st.hcmuaf.edu.vn>
2025-05-04 18:47:57 +0700

    update 4/5/25
    ud 4/5/25

--- src/main/java/com/projectttweb/webphone/service/Constants.java
+++ src/main/java/com/projectttweb/webphone/service/Constants.java
@@ -1,9 +1,9 @@
 package com.projectttweb.webphone.service;
 public class Constants {
 
-	public static String GOOGLE_CLIENT_ID = "";
+	public static String GOOGLE_CLIENT_ID = "376465340565-h50hl6rpf50vnh2ll5rh4e7qm3itk1i3.apps.googleusercontent.com";
 
-	public static String GOOGLE_CLIENT_SECRET = "";
+	public static String GOOGLE_CLIENT_SECRET = "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";
 
 	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Webphone/LoginGoogleHandler";
 
