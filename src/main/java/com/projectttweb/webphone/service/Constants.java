31b8413c4f7d813eb455480ff99bfa1ae3f6f896
Trần Anh Duy <22130063@st.hcmuaf.edu.vn>
2025-05-04 18:53:39 +0700

    update 4/5/25
    update 4/5/25

--- src/main/java/com/projectttweb/webphone/service/Constants.java
+++ src/main/java/com/projectttweb/webphone/service/Constants.java
@@ -1,16 +1,12 @@
 package com.projectttweb.webphone.service;
+
 public class Constants {
 
-	public static String GOOGLE_CLIENT_ID = "376465340565-h50hl6rpf50vnh2ll5rh4e7qm3itk1i3.apps.googleusercontent.com";
+	public static final String GOOGLE_CLIENT_ID = System.getenv("GOOGLE_CLIENT_ID");
+	public static final String GOOGLE_CLIENT_SECRET = System.getenv("GOOGLE_CLIENT_SECRET");
 
-	public static String GOOGLE_CLIENT_SECRET = "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";
-
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Webphone/LoginGoogleHandler";
-
-	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-
-	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-
-	public static String GOOGLE_GRANT_TYPE = "authorization_code";
-
+	public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/Webphone/LoginGoogleHandler";
+	public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+	public static final String GOOGLE_GRANT_TYPE = "authorization_code";
 }
